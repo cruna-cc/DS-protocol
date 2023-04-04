bc919306fe2ff8d6d77210644e660888c8c44379
Francesco Sullo <sullof@gmail.com>
2023-04-04 13:45:01 -0700

    Removing ERC165 from required explicit dependencies

--- contracts/ERC721SubordinateUpgradeable.sol
+++ contracts/ERC721SubordinateUpgradeable.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/introspection/ERC165Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 import "./IERC721SubordinateUpgradeable.sol";
